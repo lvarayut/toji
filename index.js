e768f07e5ce92846c66a3e1dae4b7159ca4a634e
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-05-26 21:56:09 +0700

    Separate build scripts for different OS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menubar = require('menubar');
 const ipc = require('electron').ipcMain;
-const mb = menubar({ preloadWindow: true, icon: __dirname + '/IconTemplate.png' });
+const path = require('path');
+const mb = menubar({ preloadWindow: true, icon: path.join(__dirname, 'icons', 'IconTemplate.png') });
 
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
