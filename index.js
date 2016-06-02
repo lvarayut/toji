0f771343ebb23ea5e13f70bcfdc3763ce54f76b1
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-06-02 21:10:11 +0700

    Move all files related to app to the app folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const menubar = require('menubar');
 const ipc = require('electron').ipcMain;
 const path = require('path');
-const mb = menubar({ preloadWindow: true, icon: path.join(__dirname, 'icons', 'IconTemplate.png') });
+const mb = menubar({ dir: path.join(__dirname, 'app'), preloadWindow: true, icon: path.join(__dirname, 'app', 'icons', 'IconTemplate.png') });
 
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
