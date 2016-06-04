e7e338d3ad2653fc8e54bbe273c89bf86140e274
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-06-04 23:41:24 +0700

    Register a global shortcut

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const menubar = require('menubar');
-const ipc = require('electron').ipcMain;
+const electron = require('electron');
 const path = require('path');
-const mb = menubar({ dir: path.join(__dirname, 'app'), preloadWindow: true, icon: path.join(__dirname, 'app', 'icons', 'IconTemplate.png') });
 
-// adds debug features like hotkeys for triggering dev tools and reload
+const ipc = electron.ipcMain;
+const globalShortcut = electron.globalShortcut;
+const mb = menubar({ dir: path.join(__dirname, 'app'), preloadWindow: true, icon: path.join(__dirname, 'app', 'icons', 'IconTemplate.png') });
+const app = mb.app;
+
 require('electron-debug')();
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
+
+app.on('ready', () => {
+  // register a show/hide window hotkey
+  globalShortcut.register('Control+space', () => {
+    if (mb.window.isVisible()) mb.hideWindow();
+    else mb.showWindow();
+  });
+});
 
 ipc.on('hide', () => {
   mb.hideWindow();
