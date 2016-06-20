af6e39fe1de3a3d498c288613ce0a43e1718c679
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2016-06-20 20:18:14 +0700

    Bind arrow keys

--- app/renderer.js
+++ app/renderer.js
@@ -75,3 +75,18 @@ quitElem.onclick = () => {
   ipc.send('quit');
 };
 
+document.onkeydown = (e) => {
+  e.preventDefault();
+
+  switch (e.which || e.keyCode) {
+    case 37: // left
+      break;
+    case 38: // up
+      break;
+    case 39: // right
+      break;
+    case 40: // down
+      break;
+    default: return;
+  }
+};
